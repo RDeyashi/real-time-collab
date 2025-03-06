81436a6ffdfd1bfdae797ded4faba44ccda89fdd
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-03-07 01:36:08 +0530

    User messaging done

--- real-time-collab-frontend/src/app/collab/chat/chat.component.ts
+++ real-time-collab-frontend/src/app/collab/chat/chat.component.ts
@@ -1,16 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
-  selectedUser: number = 0
+  messageForm: FormGroup = new FormGroup({
+    messageText: new FormControl("")
+  })
+  selectedUser: number = 0;
+  //messageText!: string | null
+  MESSAGE: any = [
+    {
+      message: 'Hi User',
+      user: 'Rabi Deyashi'
+    }
+  ]
   USER: any = [
     {
       name: "Rabi Deyashi",
@@ -74,5 +87,14 @@ export class ChatComponent {
 
   onClickUser(index: number) {
     this.selectedUser = index;
+  }
+
+  sendMessage() {
+    console.log(this.messageForm.value.messageText)
+    this.MESSAGE.push({
+      message: this.messageForm.value.messageText,
+      user: "self"
+    })
+    this.messageForm.get("messageText")?.setValue("");
   }
 }
