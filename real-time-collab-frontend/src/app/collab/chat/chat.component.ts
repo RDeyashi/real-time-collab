c83736e607123910b56701749ae1f27fb61d85f8
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-03-06 23:34:16 +0530

    header done

--- real-time-collab-frontend/src/app/collab/chat/chat.component.ts
+++ real-time-collab-frontend/src/app/collab/chat/chat.component.ts
@@ -1,11 +1,78 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  selectedUser: number = 0
+  USER: any = [
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    }, {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    }, {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    },
+    {
+      name: "Rabi Deyashi",
+      lastMsg: "I'll be OOO",
+      logo: ''
+    }
+  ]
 
+  onClickUser(index: number) {
+    this.selectedUser = index;
+  }
 }
