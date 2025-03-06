4318a847692f48c2eb7b4c484d42522fcd177b2a
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-03-06 19:38:14 +0530

    Signin form done

--- real-time-collab-frontend/src/app/signin/signin.component.ts
+++ real-time-collab-frontend/src/app/signin/signin.component.ts
@@ -1,16 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { EncryptDecryptService } from '../service/encrypt-decrypt.service';
 
 @Component({
   selector: 'app-signin',
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+  constructor(private encDecService: EncryptDecryptService){}
+  signin:FormGroup = new FormGroup({
+    userName: new FormControl(""),
+    password: new FormControl("", [Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)])
+  })
 
+  async onClickSignIn(){
+    const encPaswd = await this.encDecService.encryptPassword(this.signin.value.password)
+    this.signin.get("password")?.setValue(encPaswd)
+
+    console.log(this.signin.value)
+  }
 }
