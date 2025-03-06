226b5dae73b44e9d00207dc42714f60680b9fde2
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-03-06 17:43:07 +0530

    signin done

--- real-time-collab-frontend/src/app/signup/signup.component.ts
+++ real-time-collab-frontend/src/app/signup/signup.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
