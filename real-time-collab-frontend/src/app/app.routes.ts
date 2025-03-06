04796d13b198f28cabdc44ceff2e88928d9657d5
Rabi Deyashi <engg.r.deyashi@gmail.com>
2025-03-06 17:29:29 +0530

    signup done

--- real-time-collab-frontend/src/app/app.routes.ts
+++ real-time-collab-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/signup', pathMatch: 'full' },
+    { path: 'signup', component: SignupComponent },
+    { path: 'signin', component: SigninComponent }
+];
